f1cdbb1a1d7ea66e7f51b45ec844ef63872df068
jcvidela <juancarlos.videla00@gmail.com>
2020-11-14 02:11:16 -0300

    colors constant

---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+export const CONTAINER_BACKGROUND_COLOR = '#ebf6fa';
+export const SEARCHBAR_FONT_COLOR = '#fff';
+export const LIST_ITEM_BACKGROUND_COLOR = '#fff';
+export const ACTIVITY_INDICATOR_COLOR = '#00aded';
+export const INPUT_FORM_BORDER_COLOR = '#ccc';
+export const SIGNIN_BUTTON_FONT_COLOR = '#fff';
+export const SIGNIN_BUTTON_BACKGROUND_COLOR = '#00aded';
+export const SIGNIN_BUTTON_BACKGROUND_COLOR_DISABLED = '#ccc';
