17985f905703c4e34a7482deb14d4518013f38be
jcvidela <juancarlos.videla00@gmail.com>
2020-11-17 01:09:23 -0300

    url's api

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV;
 let baseURL = '';
 
-env === 'development' ? baseURL = 'http://192.168.100.224:3000' : baseURL = 'http://localhost:3000';
+env === 'development' ? baseURL = 'http://192.168.100.224:3000' : baseURL = 'https://5fb30684b6601200168f6f5b.mockapi.io';
 
 export default baseURL;
